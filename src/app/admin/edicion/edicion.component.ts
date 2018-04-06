061acd951ac983291574e1be72ad377b2acb94a9
Jorge Fernandez <bitsingen@gmail.com>
2018-04-06 16:00:58 +0200

    version marzo2018 2

--- src/app/admin/edicion/edicion.component.ts
+++ src/app/admin/edicion/edicion.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { CiudadesService } from '../../api/ciudades.service';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-edicion',
   templateUrl: './edicion.component.html',
-  styleUrls: ['./edicion.component.css']
+  styleUrls: ['./edicion.component.css'],
+  providers:[CiudadesService, AngularFireDatabase]
 })
 export class EdicionComponent implements OnInit {
+  fullcities:any =[];
+  grupociudades:any=[];
+  popup:boolean = false;  
+  constructor(public apicity:CiudadesService) { }
 
-  constructor() { }
 
+  popupinfo:any = {}
+  
+    AbrirPopup:any = function(paket){
+      this.popup = true;
+      console.log(paket.proyecto);
+      this.popupinfo.proyecto = paket.proyecto;
+      this.popupinfo.tipo = paket.tipo;
+      this.popupinfo.id = paket.id;
+      this.popupinfo.tipociudad = paket.tipociudad;
+  
+    }
+  
+    CerrarPopup:any = function(){
+      this.popup=false;
+    }
+    
   ngOnInit() {
+    this.apicity.CargaDb().subscribe((paquete)=>{
+      this.fullcities = [];
+    this.grupociudades = paquete;
+    for(let item of paquete){
+      this.fullcities.push(item)
+    }
+    console.log(this.grupociudades.length);
+  });
   }
 
 }
